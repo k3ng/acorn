78b572f68ec9c765256ba8caa004bb9decd47751
k3ng <anthony.good@gmail.com>
2023-02-22 22:54:27 -0500

    2022-02-22 2254 work

--- tcpclient.h
+++ tcpclient.h
@@ -5,8 +5,15 @@
 
 #define MAX_TCPCLIENTS 16
 
-int tcpclient_open(int tcpclient_slot, char *server);
-int tcpclient_write(int tcpclient_slot, char *text);
-int tcpclient_close(int tcpclient_slot);
+int tcpclient_open(char *server);
+int tcpclient_write(int tcpclient_handle, char *text);
+int tcpclient_close(int tcpclient_handle);
+
+struct tcpclient_parms_struct{
+	char *server;
+	int tcpclient_handle;
+};
+
+int tcpclient_handle_tcp_sock_array[MAX_TCPCLIENTS];
 
 #endif //!defined(_tcpclient_h_)
