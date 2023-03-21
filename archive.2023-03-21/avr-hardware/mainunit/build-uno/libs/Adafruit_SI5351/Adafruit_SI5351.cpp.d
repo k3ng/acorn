f5721a86380847f6849336582791916c12ad289e
k3ng <anthony.good@gmail.com>
2023-03-21 10:57:52 -0400

    archiving stuff 2

---
+++ archive.2023-03-21/avr-hardware/mainunit/build-uno/libs/Adafruit_SI5351/Adafruit_SI5351.cpp.d
@@ -0,0 +1,18 @@
+build-uno/libs/Adafruit_SI5351/Adafruit_SI5351.cpp.o: \
+ /usr/share/arduino/libraries/Adafruit_SI5351/Adafruit_SI5351.cpp \
+ /usr/share/arduino/libraries/Adafruit_SI5351/Adafruit_SI5351.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino//variants/standard/pins_arduino.h \
+ /usr/share/arduino/libraries/Adafruit_I2CDevice/Adafruit_I2CDevice.h \
+ /usr/share/arduino/libraries/Wire/Wire.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Stream.h \
+ /usr/share/arduino/libraries/Adafruit_SI5351/asserts.h \
+ /usr/share/arduino/libraries/Adafruit_SI5351/errors.h
